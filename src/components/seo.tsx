df68981047b09c82ef547ad6c86a374738448a0c
sijin <skd6150@gmail.com>
2022-11-28 16:06:46 +0900

    react-helmet, seo 적용

---
+++ src/components/seo.tsx
@@ -0,0 +1,67 @@
+import * as React from "react";
+import { Helmet } from "react-helmet";
+import { useStaticQuery, graphql } from "gatsby";
+
+interface SeoProps {
+  title: string;
+}
+
+const Seo = ({ title }: SeoProps) => {
+  const { site } = useStaticQuery(
+    graphql`
+      query {
+        site {
+          siteMetadata {
+            title
+            description
+          }
+        }
+      }
+    `
+  );
+
+  const metaDescription = site.siteMetadata.description;
+  const defaultTitle = site.siteMetadata?.title;
+
+  return (
+    <Helmet
+      htmlAttributes={{
+        lang: "en",
+      }}
+      title={title}
+      titleTemplate={defaultTitle ? `%s | ${defaultTitle}` : undefined}
+      meta={[
+        {
+          name: `description`,
+          content: metaDescription,
+        },
+        {
+          property: `og:title`,
+          content: title,
+        },
+        {
+          property: `og:description`,
+          content: metaDescription,
+        },
+        {
+          property: `og:type`,
+          content: `website`,
+        },
+        {
+          name: `twitter:card`,
+          content: `summary`,
+        },
+        {
+          name: `twitter:title`,
+          content: title,
+        },
+        {
+          name: `twitter:description`,
+          content: metaDescription,
+        },
+      ]}
+    />
+  );
+};
+
+export default Seo;
